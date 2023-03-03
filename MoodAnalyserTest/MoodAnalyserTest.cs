641f6a0c39779db9f307a99210827794964c87e0
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-03-03 09:03:01 +0530

    Modified to Refactor UC1 To Use Paraneterised Constructor Test Case

--- MoodAnalyserTest/MoodAnalyserTest.cs
+++ MoodAnalyserTest/MoodAnalyserTest.cs
@@ -12,5 +12,13 @@ namespace MoodAnalyserTest
             Assert.AreEqual(result, "Sad");
             Assert.Pass();
         }
+        [Test]
+        public void GivenSadMood_WhenAnalysed_SholudReturnHappy()
+        {
+            MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
+            string result = moodAnalyser.AnalyseMood();
+            Assert.AreEqual(result, "Happy");
+            Assert.Pass();
+        }
     }
 }
